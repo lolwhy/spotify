94d072d2d8bd5d76fdacf29271437ea3a38b14ae
lolwhy <gioiaabest@gmail.com>
2021-05-02 19:48:14 +0200

    Finish

--- spotify/transfer_playback.js
+++ spotify/transfer_playback.js
@@ -1,12 +1,16 @@
 const { spotifyApi } = require('../app.js');
 
 const transferPlayback = async (availableDevices) => {
-  spotifyApi.transferMyPlayback(availableDevices)
-    .then(() => {
-      console.log(`Transfering playback to ${availableDevices}`);
-    }, (err) => {
+  const availableDevice = Array.from(availableDevices[0]);
+  console.log('AVAIL DEVICE', availableDevice);
+  spotifyApi.transferMyPlayback(availableDevice).then(
+    () => {
+      console.log(`Transfering playback to ${availableDevice}`);
+    },
+    (err) => {
       console.log('Transfering playback failed: ', err.body);
-    });
+    },
+  );
 };
 
 module.exports = transferPlayback;
